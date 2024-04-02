70bb0cc2783d4ba384c04c4fc78dd5a44d9c9544
timax <timax@LAPTOP-SLL8FUPO>
2024-04-02 10:44:04 +0300

    Добавьте файлы проекта.

---
+++ Controllers/SiteRegistrationController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FinancialAccounting.Models;
+using FinancialAccounting.Data;
+
+namespace FinancialAccounting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SiteRegistrationController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public SiteRegistrationController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        // Creat/Edit
+        [HttpPost]
+        public JsonResult CreateEdit(SiteRegistration siteRegistration)
+        {
+            if (siteRegistration.Id == 0)
+            {
+                _context.Registrations.Add(siteRegistration);
+            }
+            else
+            {
+                var registrationInDb = _context.Registrations.Find(siteRegistration);
+
+                if (registrationInDb == null)
+                    return new JsonResult(NotFound());
+                registrationInDb = siteRegistration;
+            }
+
+            _context.SaveChanges();
+            return new JsonResult(Ok(siteRegistration));
+        }
+
+        [HttpGet]
+        public JsonResult Get(int id)
+        {
+            var result = _context.Registrations.Find(id);
+
+            if (result == null)
+                return new JsonResult(NotFound());
+
+            return new JsonResult(Ok(result));
+        }
+        [HttpDelete]
+        public JsonResult Delete(int id)
+        {
+            var result = _context.Registrations.Find(id);
+
+            if (result == null)
+                return new JsonResult(NotFound());
+
+            _context.Registrations.Remove(result);
+            _context.SaveChanges();
+            return new JsonResult(NoContent());
+        }
+    }
+}
---
+++ Data/ApiContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinancialAccounting.Models;
+namespace FinancialAccounting.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<SiteRegistration> Registrations { get; set; }
+        public ApiContext(DbContextOptions<ApiContext> options) :base(options)
+        {
+
+        }
+    }
+}
